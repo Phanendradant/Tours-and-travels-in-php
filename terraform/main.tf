d336c60a033f3fffe2c4a954de8b7d2a54c4150e
Phanendradant <152456927+Phanendradant@users.noreply.github.com>
2024-08-24 19:03:56 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,24 +13,49 @@ resource "aws_vpc" "main_vpc" {
   }
 }
 
-# Public Subnet
-resource "aws_subnet" "public_subnet" {
+# Public Subnet in AZ1
+resource "aws_subnet" "public_subnet_az1" {
   vpc_id                  = aws_vpc.main_vpc.id
   cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-west-2a"
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "public_subnet"
+    Name = "public_subnet_az1"
   }
 }
 
-# Private Subnet
-resource "aws_subnet" "private_subnet" {
-  vpc_id     = aws_vpc.main_vpc.id
-  cidr_block = "10.0.2.0/24"
+# Public Subnet in AZ2
+resource "aws_subnet" "public_subnet_az2" {
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-west-2b"
+  map_public_ip_on_launch = true
 
   tags = {
-    Name = "private_subnet"
+    Name = "public_subnet_az2"
+  }
+}
+
+# Private Subnet in AZ1
+resource "aws_subnet" "private_subnet_az1" {
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "us-west-2a"
+
+  tags = {
+    Name = "private_subnet_az1"
+  }
+}
+
+# Private Subnet in AZ2
+resource "aws_subnet" "private_subnet_az2" {
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = "10.0.4.0/24"
+  availability_zone = "us-west-2b"
+
+  tags = {
+    Name = "private_subnet_az2"
   }
 }
 
@@ -43,7 +68,7 @@ resource "aws_internet_gateway" "igw" {
   }
 }
 
-# Route Table for Public Subnet
+# Route Table for Public Subnets
 resource "aws_route_table" "public_rt" {
   vpc_id = aws_vpc.main_vpc.id
 
@@ -57,8 +82,13 @@ resource "aws_route_table" "public_rt" {
   }
 }
 
-resource "aws_route_table_association" "public_rt_assoc" {
-  subnet_id      = aws_subnet.public_subnet.id
+resource "aws_route_table_association" "public_rt_assoc_az1" {
+  subnet_id      = aws_subnet.public_subnet_az1.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "public_rt_assoc_az2" {
+  subnet_id      = aws_subnet.public_subnet_az2.id
   route_table_id = aws_route_table.public_rt.id
 }
 
@@ -107,7 +137,12 @@ module "eks" {
   cluster_name    = "my-cluster"
   cluster_version = "1.26"
   vpc_id          = aws_vpc.main_vpc.id
-  subnets         = [aws_subnet.public_subnet.id, aws_subnet.private_subnet.id]
+  subnets         = [
+    aws_subnet.public_subnet_az1.id, 
+    aws_subnet.public_subnet_az2.id, 
+    aws_subnet.private_subnet_az1.id, 
+    aws_subnet.private_subnet_az2.id
+  ]
 
   worker_groups = [
     {
