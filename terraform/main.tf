f9f9b7c598a0d0d83a64411b19e69185615c013d
Phanendradant <152456927+Phanendradant@users.noreply.github.com>
2024-08-24 19:23:48 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -160,3 +160,18 @@ module "eks" {
     Project     = "my-project"
   }
 }
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.main.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.main.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.main.token
+}
+
+data "aws_eks_cluster" "main" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "main" {
+  name = module.eks.cluster_id
+}
+
