78c72d1ed73317808e128eb3b10f66cdb028a3de
Phanendradant <152456927+Phanendradant@users.noreply.github.com>
2024-08-25 09:27:16 +0530

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -23,13 +23,13 @@ chmod +x kubectl
 sudo mv kubectl /usr/local/bin/
 
 # Install Jenkins
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
-echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
-sudo apt-get update -y
-sudo apt-get install -y openjdk-11-jdk
-sudo apt-get install -y jenkins
-sudo systemctl start jenkins
-sudo systemctl enable jenkins
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt-get install jenkins
 
 
 # Add Jenkins user to Docker group
